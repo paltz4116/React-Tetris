0e50f4e0118a43d556b0a1c109ed569aa531c349
pfalz <mega75581@gmail.com>
2023-11-26 17:38:04 +0900

    added styles

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -1,12 +1,17 @@
 import React from "react";
 
+import classes from "./Board.module.css";
+
 const Board = (props) => {
   return (
-    <div className="board">
+    <div className={classes.board}>
       {props.board.map((row, rowIndex) => (
-        <div key={rowIndex} className="row">
+        <div key={rowIndex} className={classes.row}>
           {row.map((cell, colIndex) => (
-            <div key={colIndex} className={`cell ${cell ? "block" : ""}`}></div>
+            <div
+              key={colIndex}
+              className={`${classes.cell} ${cell ? classes.block : ""}`}
+            ></div>
           ))}
         </div>
       ))}
